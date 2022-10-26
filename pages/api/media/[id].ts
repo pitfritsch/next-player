8a5fe596f904d72c348462a5722eb34b4d061ea3
Pedro Fritsch <pedrofritsch98@gmail.com>
2022-10-26 11:01:08 +1000

    Correct rejecting

--- pages/api/media/[id].ts
+++ pages/api/media/[id].ts
@@ -1,6 +1,6 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import http from "http";
 import https from "https";
+import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   const source = Buffer.from(String(req.query.id), "base64").toString("utf-8").trim();
@@ -8,7 +8,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<any>) 
   const isHttps = source.startsWith("https");
   const protocol = isHttps ? https : http;
 
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     try {
       // const { host, accept, "accept-encoding", "accept-language",  } = req.headers;
       const rawHeaders = {
@@ -36,20 +36,24 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<any>) 
           headers: newHeaders,
         },
         function (response) {
-          const newHeaders = { ...response.headers };
-          if (newHeaders.location)
-            newHeaders.location = `/api/media/${Buffer.from(newHeaders.location, "utf-8").toString(
-              "base64"
-            )}`;
-          res.writeHead(response.statusCode || 400, response.statusMessage, newHeaders);
-          response.pipe(res);
-          // response.on("end", res)
-          resolve(0);
+          try {
+            const newHeaders = { ...response.headers };
+            if (newHeaders.location)
+              newHeaders.location = `/api/media/${Buffer.from(
+                newHeaders.location,
+                "utf-8"
+              ).toString("base64")}`;
+            res.writeHead(response.statusCode || 400, response.statusMessage, newHeaders);
+            response.pipe(res);
+            // response.on("end", res)
+            resolve(0);
+          } catch (error) {
+            reject(error);
+          }
         }
       );
     } catch (error: any) {
-      console.log(`catch`, error);
-      res.status(400).send({ message: error.message, error });
+      reject(error);
     }
   }).catch((e) => {
     console.log(e);
